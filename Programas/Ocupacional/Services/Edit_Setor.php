19a86e68a368b3e5d58203a406903207b90aae05
datarowinfo <datarowinfo@gmail.com>
2023-10-24 02:54:46 -0300

    Criando o update de edição do cadastro do setor.

--- Programas/Ocupacional/Services/Edit_Setor.php
+++ Programas/Ocupacional/Services/Edit_Setor.php
@@ -7,11 +7,14 @@ $descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_STRING);
 $caracterizacao = filter_input(INPUT_POST, 'caracterizacao', FILTER_SANITIZE_STRING);
 $acao = filter_input(INPUT_POST, 'Subject', FILTER_SANITIZE_NUMBER_INT);
 
-//echo "Nome: $nome <br>";
-//echo "E-mail: $email <br>";
 switch($acao){
 	case "1":
-		$result_usuario = "UPDATE tb_hierarquia SET descricao='$descricao', caracaterizacao='$caracterizacao', datamodificacao=NOW() WHERE id_hierarquia='$id'";
+		$result_usuario = "UPDATE adeluc.tb_hierarquia "
+                . "SET descricao='$descricao' , "
+                . "caracterizacao='$caracterizacao', "
+                . "datamodificacao=NOW() "
+                . "WHERE id_hierarquia='$id'";
+            
                 $resultado_usuario = mysqli_query($con, $result_usuario);
 
                 if(mysqli_affected_rows($con)){
@@ -19,7 +22,7 @@ switch($acao){
                     header("Location: ../View/setor.php");
                 }else{
                         $_SESSION['msg'] = "<p style='color:red;'>Usuário não foi editado com sucesso</p>";
-                        header("Location: ../View/Edit_Setor.php?id=$id");
+                        header("Location: ../View/Setor.php?id=$id");
                 }
                 break;
 
