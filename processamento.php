e539a14cc28c8d94a27d141f1bc02ad40b4b55f5
datarowinfo <datarowinfo@gmail.com>
2024-01-07 10:52:09 -0300

    Pagina de processamento.

--- processamento.php
+++ processamento.php
@@ -10,7 +10,7 @@
     
     <!-- Estilos customizados para esse template -->
     <link href="fonts/signin.css" rel="stylesheet">
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <meta http-equiv="refresh" content="2; URL='http://localhost/DTR-ADELUC/app.php'"/>
 
     <title>LOGIN</title>
@@ -24,9 +24,10 @@
   <h1 class="h3 mb-3 font-weight-normal"><img src="image/LogoLogin.png"></h1>
         
         
-         <div class="d-flex align-items-center">
-  <strong><i class="fa fa-spinner" aria-hidden="true"></i></strong>
-</div>
+         <div class="fa-3x text-secondary">
+       <i class="fa fa-spinner fa-spin fa-2x fa-fw"></i>
+        <span class="sr-only">Loading...</span>
+        </div>
            
     </form>
 </div>
